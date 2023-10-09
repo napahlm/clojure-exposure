1013bdfe18026083c4b435cadc9887826c62a373
Pahlm <borgepahlm@gmail.com>
2023-10-09 16:54:10 +0200

    Update gitignore

--- resources/public/js/compiled/cljs-runtime/clojure_exposure.config.js
+++
@@ -1,4 +0,0 @@
-goog.provide('clojure_exposure.config');
-clojure_exposure.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=clojure_exposure.config.js.map
--- resources/public/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { clojure_exposure.core.init(); } catch (e) { console.error("An error occurred when calling (clojure-exposure.core/init)"); throw(e); }
